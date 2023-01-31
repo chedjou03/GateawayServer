dbaec4f75c51cc3ac172257d0b975a8a577c5aa5
Simplice chedjou <chedjou03@yahoo.fr>
2023-01-30 19:43:55 -0600

    add sercurity to the gateway project

---
+++ src/main/java/com/gatewayserver/configuration/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.gatewayserver.configuration;
+
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    //define my security requirement here
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+        http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/chedjoubank/accounts/**").authenticated()
+                        .pathMatchers("/chedjoubank/cards/**").authenticated()
+                        .pathMatchers("/chedjoubank/loans/**").permitAll())
+                .oauth2ResourceServer().jwt();//.jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+        http.csrf().disable();
+        return http.build();
+
+    }
+
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+}
